25b320cc1fcf2a3e3d4656ecdce3774dda99f730
Wolke <wolkesau@gmail.com>
2020-11-12 13:45:06 +0800

    4-7

--- src/main.js
+++ src/main.js
@@ -1,12 +1,32 @@
 const EXCHANGE_RATE = 30;
-
 class Agent {
-  constructor(name, age) {
+  constructor(name, age, weight) {
     this.name = name;
     this.age = age;
+    this.weight = weight;
   }
   countExchange(money) {
     return EXCHANGE_RATE * money;
+  }
+  responseDate() {
+    let today = new Date();
+    let s = today.toLocaleString("zh-TW", {
+      timezone: "Asia/Taipei"
+    });
+    let d = s.split("/");
+    // d = d.push(1);
+    // console.log(d, d.push(2));
+
+    return `今天是${d[0]}月${d[1]}日`;
+  }
+
+  responseGetFundDate() {
+    let today = new Date();
+    console.log(today.getTime());
+    let dt = today.getTime() + 24 * 60 * 60 * 1000 * 7;
+    console.log(dt);
+    let end = new Date(dt);
+    return `${end.getMonth() + 1}月 ${end.getDate()} 日拿錢`;
   }
 }
 
@@ -21,14 +41,12 @@ class Major extends Agent {
   }
 }
 
-let amy = new Agent("Amy", 28);
-let peter = new Major("Peter", 50);
-console.log(typeof amy);
+let amy = new Agent("Amy", 28, 49.8);
+let peter = new Major("Peter", 50, 53.3);
+// console.log(typeof amy);
 
 function handle(text) {
-  // amy.countExchange = (...money) => EXCHANGE_RATE * money + 1;
-  // peter.countExchange = null;
-  return peter.drinkPot(text);
+  return amy.responseDate();
 }
 
 module.exports = handle;
