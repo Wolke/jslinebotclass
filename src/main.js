0220d560a77b574f03ef0c2ba3019c99e10011c3
Wolke <wolkesau@gmail.com>
2020-12-04 17:07:54 +0800

    5-2

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 function handle(text) {
-  return text;
+  let r = `白天好!`;
+  let d = new Date();
+
+  let s = d.toLocaleString(`zh-TW`, { timeZone: `Asia/Taipei`, hour12: false });
+  console.log(s);
+  let t = s.slice(11, 13);
+
+  if (t > 5 && t < 11) {
+    r = `早安!`;
+  } else if (t >= 11 && t <= 16) {
+    r = `午安!`;
+  } else {
+    r = `晚安!`;
+  }
+  return r;
 }
 
 module.exports = handle;
