f83df8d23665079921abbfe16e82fcf4e3093c61
Wolke <wolkesau@gmail.com>
2020-12-11 11:14:49 +0800

    6-2 6-2

--- src/main.js
+++ src/main.js
@@ -1,24 +1,3 @@
-var ary = [`amy`, `peter`, `john`];
-
-let r = ary.reduce((p, v, i, a) => {
-  console.log(p, v, i, a);
-  // return v;
-  if (p.indexOf(`a`) > -1) {
-    return p;
-  }
-  if (v.indexOf(`a`) > -1) {
-    return v;
-  }
-});
-// console.log(r);
-
-// let r = [];
-for (let i = 0, j = 0; i < ary.length; i++) {
-  if (ary[i].indexOf(`a`) > -1) {
-    r.push(ary[j]);
-  }
-}
-
 function handle(text) {
   return text;
 }
