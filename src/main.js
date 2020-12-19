9d010fef0f4e796c63c34a1e110eb7b7b54424c7
Wolke <wolkesau@gmail.com>
2020-12-19 11:03:47 +0800

    10-4

--- src/main.js
+++ src/main.js
@@ -1,41 +1,31 @@
-// number
-//user = > number
-const { GoogleSpreadsheet } = require("google-spreadsheet");
-var getNumber = async () => {
-  const doc = new GoogleSpreadsheet(
-    "1KU0PsZ2udpWjwGy1aw8uajHzM1IGMHidcf26O88hJWA"
-  );
-  doc.useServiceAccountAuth({
-    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n")
-  });
-  await doc.loadInfo();
-  // console.log(doc.title);
-  let sheet = doc.sheetsByIndex[0];
-  await sheet.loadCells("A1:E10");
-  let a1 = sheet.getCell(0, 0);
-  console.log(a1.value);
-  if (a1.value === null) {
-    a1.value = Math.floor(Math.random() * 100) + 1;
-    await sheet.saveUpdatedCells();
+/*
+flow
+好物 => [...]
+好店 => [...]
+好宿 => [...]
+交通 => imagemenu
+...
+*/
+
+function handle(text) {
+  let r = ``;
+  switch (text) {
+    case "好物":
+      r = "好物...";
+      break;
+    case "好店":
+      r = "好店...";
+      break;
+    case "好宿":
+      r = "好宿..";
+      break;
+    case "交通":
+      r = "交通..";
+      break;
+    default:
+      break;
   }
-
-  return a1.value;
-};
-// getNumber();
-
-async function handle(text) {
-  let ans = await getNumber();
-  console.log(ans, text);
-  if (text == ans) {
-    return "correct";
-  } else if (text > ans) {
-    return "small";
-  } else if (text < ans) {
-    return "big";
-  }
-
-  return ans;
+  return r;
 }
 
 module.exports = handle;
